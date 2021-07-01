900fb57b0e8766db8f26ab7646ed7fa7503bdd67
Jitesh Doshi <jitesh@spinspire.com>
2021-07-01 18:29:41 -0400

    implement code syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
-module.exports = {
-  dir: {
-    input: "content",
-    output: "dist",
-  },
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+  return {
+    dir: {
+      input: "content",
+      output: "dist",
+    },
+  };
 };
