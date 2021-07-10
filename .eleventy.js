b3bb3b60b94fcedade980f585df38091b6dad95f
Jitesh Doshi <jitesh@spinspire.com>
2021-07-10 15:18:58 -0400

    setDataDeepMerge true

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+  // requires opt-in for 0.x
+  eleventyConfig.setDataDeepMerge(true);
   return {
     dir: {
       input: "content",
