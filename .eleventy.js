99e20be72783bab2217215837c843643fdd93467
Jitesh Doshi <jitesh@spinspire.com>
2021-07-10 15:20:59 -0400

    addCollection untagged

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   // requires opt-in for 0.x
   eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addCollection("untagged", function (collection) {
+    const all = collection.getAll();
+    const untagged = all.filter(page => page.data.tags === undefined);
+    return untagged;
+  });
+
   return {
     dir: {
       input: "content",
