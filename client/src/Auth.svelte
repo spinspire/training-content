e1cb32e9e9e19131c96c6061502bfc0f7229e43f
Jitesh Doshi <jitesh@spinspire.com>
2021-07-20 14:07:51 -0400

    add client app

---
+++ client/src/Auth.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<div>Hello {name}!</div>
+</main>
---
+++ client/src/main.ts
@@ -0,0 +1,10 @@
+import Auth from './Auth.svelte';
+
+new Auth({
+	target: document.getElementById("auth"),
+	props: {
+		name: 'TheUser'
+	}
+});
+
+// export default app;
