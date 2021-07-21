4630bf61010efe7ba44841057442bc1f0741973d
Brody Faust <bfwolfgangfaust@gmail.com>
2021-07-21 19:43:22 -0400

    added placeholder login, logout, register buttons

--- client/src/main.js
+++ client/src/main.js
@@ -3,12 +3,8 @@ import Auth from './Auth.svelte';
 new Auth({
 	target: document.getElementById('auth'),
 	props: {
-<<<<<<< Updated upstream
-		name: 'TheUser',
-=======
 		name: 'User',
 		loggedIn: false,
->>>>>>> Stashed changes
 	},
 });
 
