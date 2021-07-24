be159a01366237c4ef5c9bfb7c9165022b1a1eb3
Jitesh Doshi <jitesh@spinspire.com>
2021-07-24 13:52:41 -0400

    properly disable YouTube API calls

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,4 +2,8 @@ module.exports = {
   name: "SpinSpire Learning",
   parent: "https://spinspire.com",
   // articles: "https://spinspire.com/jsonapi/node/article",
+  youtube: {
+    channelId: "UCJ3h7utY-IjXKXOUuwmSYXw",
+    // apiKey: "...",
+  }
 };
--- src/video/video.11tydata.js
+++ src/video/video.11tydata.js
@@ -1,11 +1,11 @@
 const fetch = require("node-fetch");
+const site = require("../_data/site");
 
-const apiKey = "AIzaSyAQCcE2dquCovM9xw_g2-oW74VcAG2i31c";
-const channelId = "UCJ4h7utY-IjXKXOUuwmSYXw";
+const apiKey = site.youtube.apiKey;
+const channelId = site.youtube.channelId;
 
 module.exports = async function() {
-  // disabled for now because we don't want to make actual YoutTube API calls and exhaust our quota
-  return {};
+  if(!apiKey) return {}; // disable if no apiKey
 
   const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=200`;
   const response = await fetch(url);
