5e1ca5a88deec57bbc5e11b595456a015d09b97c
Jitesh Doshi <jitesh@spinspire.com>
2021-07-26 13:54:26 -0400

    Auth component: fix layout

--- client/src/Auth.svelte
+++ client/src/Auth.svelte
@@ -2,26 +2,30 @@
   import { user } from "./lib/firebase";
 </script>
 
-<nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      {#if $user }
-        <li class="nav-item ">
-          <p class="h1">{$user.displayName}</p>
-        </li>
-        <img
-          class="profile-pic"
-          src={$user.photoURL}
-          alt="{$user.displayName}'s profile picture."
-        />
-        <li class="nav-item">
-          <button on:click|preventDefault={user.logout}>Log Out</button>
-        </li>
-      {:else}
-        <li class="nav-item">
-          <button on:click|preventDefault={user.login}>Login</button>
-        </li>
-      {/if}
-    </ul>
-  </div>
-</nav>
+<div class="btn-group firebase-auth">
+  {#if $user}
+    <img class="profile-pic"
+      src={$user.photoURL}
+      alt="{$user.displayName}'s profile picture."
+      title="{$user.displayName}'s profile picture."
+    />
+    <button on:click|preventDefault={user.logout} class="btn btn-secondary">Log Out</button>
+  {:else}
+    <div class="profile-pic"></div>
+    <button on:click|preventDefault={user.login} class="btn btn-secondary">Login</button>
+  {/if}
+</div>
+
+<style>
+  .firebase-auth {
+    display: flex;
+    height: 3rem;
+  }
+  .profile-pic {
+    display: inline-block;
+    width: 3rem;
+  }
+  .btn {
+    width: 6em;
+  }
+</style>
