42cc41f81d196106cf2ee5ca9ed5929398980328
Jitesh Doshi <jitesh@spinspire.com>
2021-07-28 16:02:03 -0400

    add email signup form

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: "SpinSpire Learning",
   parent: "https://spinspire.com",
+  emailSignup: "https://cdn.forms-content.sg-form.com/313c0e55-efce-11eb-8cd5-fe481b0d36ae",
   // articles: "https://spinspire.com/jsonapi/node/article",
   youtube: {
     channelId: "UCJ3h7utY-IjXKXOUuwmSYXw",
