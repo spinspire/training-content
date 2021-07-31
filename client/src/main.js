46ec59e7b9fa7dd3b179d9567fbcbc5ff94978ad
Jitesh Doshi <jitesh@spinspire.com>
2021-07-31 09:55:33 -0400

    detect missing swup containers

--- client/src/main.js
+++ client/src/main.js
@@ -1,22 +1,30 @@
 import Auth from './Auth.svelte';
 import Swup from 'swup';
 
-// gives ajax page transitions. See https://swup.js.org/
+// swup gives ajax page transitions. See https://swup.js.org/
+const containers = [
+  '#swup',
+  'header>h1',
+  'footer span.author',
+].filter(c => { // filter out non-existent containers
+  const element = document.querySelector(c);
+  if (element) {
+    return element;
+  } else {
+    console.warn("Swup container not found: ", c);
+  }
+});
 const swup = new Swup({
-	containers: [
-		'#swup',
-		'header>h1',
-		'footer span.author',
-	]
+  containers,
 });
 
 new Auth({
-	target: document.getElementById('auth'),
-	props: {
-		// name: 'User',
-		// user: null,
-		// loggedIn: false,
-	},
+  target: document.getElementById('auth'),
+  props: {
+    // name: 'User',
+    // user: null,
+    // loggedIn: false,
+  },
 });
 
 // export default app;
