cffb21273c0b5548eff8a5862cbe471d33986ce0
Brody Faust <bfwolfgangfaust@gmail.com>
2021-08-02 12:42:52 -0400

    added firestore and user creation on DB

---
+++ client/src/lib/firebase/firestore/index.js
@@ -0,0 +1,18 @@
+import { db } from '../index';
+
+export const postLastLocation = () => {
+	const currentLocation = window.location.href;
+
+	// const locationTitle;
+	// const locationURL;
+};
+
+// console.log(db);
+// db.collection('users')
+// 	.add({ displayName: 'chris man' })
+// 	.then((docRef) => {
+// 		console.log(docRef.id);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
--- client/src/lib/firebase/index.js
+++ client/src/lib/firebase/index.js
@@ -3,36 +3,73 @@ import { readable } from 'svelte/store';
 import firebase from 'firebase/app';
 import config from './config';
 import 'firebase/auth'; // for side-effects only
+import 'firebase/firestore';
 
 const app = firebase.initializeApp(config);
 
+export const db = firebase.firestore();
+
+const users = db.collection('users');
+
+export const createUser = async (user) => {
+	try {
+		await users.doc(user.uid).set(
+			{
+				uid: user.uid,
+				displayName: user.displayName,
+			},
+			{ merge: true }
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const updateUserLocation = async () => {
+	const currentLocationURL = window.location.href;
+	const user = await firebase.auth().currentUser;
+	try {
+		await users.doc(user.uid).set(
+			{
+				locationURL: currentLocationURL,
+				locationTitle: document.title,
+			},
+			{ merge: true }
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
 // 'user' is a readable store that is tied to the auth status of the firebase user
 export const user = readable(null, (setter) => {
-  // listen for login/logout events and update user store
-  firebase.auth().onAuthStateChanged(u => {
-    setter(u);
-  });
+	// listen for login/logout events and update user store
+	firebase.auth().onAuthStateChanged((u) => {
+		setter(u);
+	});
 });
 
 // extra method on the store
 user.login = async function () {
-  try {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    await firebase.auth().signInWithPopup(provider);
-  } catch (e) {
-    const message = `${e.code}\n${e.message}`;
-    console.error(message);
-    alert(message);
-  }
+	try {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		await firebase.auth().signInWithPopup(provider);
+		const user = await firebase.auth().currentUser;
+		await createUser(user);
+	} catch (e) {
+		const message = `${e.code}\n${e.message}`;
+		console.error(message);
+		alert(message);
+	}
 };
 
 // extra method on the store
 user.logout = async function () {
-  try {
-    await firebase.auth().signOut();
-  } catch (e) {
-    const message = `${e.code}\n${e.message}`;
-    console.error(message);
-    alert(message);
-  }
+	try {
+		await firebase.auth().signOut();
+	} catch (e) {
+		const message = `${e.code}\n${e.message}`;
+		console.error(message);
+		alert(message);
+	}
 };
